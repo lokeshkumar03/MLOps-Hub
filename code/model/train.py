1f5a092896fd0de0d5c1b90d81480746c17bcfd1
lchikkala28 <lokeshchikkala.cas02@outlook.com>
2024-09-17 08:44:37 +0530

    updated train.py

--- code/model/train.py
+++ code/model/train.py
@@ -1,30 +1,30 @@
 # Import libraries
+
 import argparse
 import glob
 import os
+
 import pandas as pd
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from mlflow.sklearn import autolog
-from sklearn.metrics import accuracy_score
 
-# Define functions
+
+# define functions
 def main(args):
-    # Enable autologging
+    # TO DO: enable autologging
     autolog()
 
-    # Read data
+    # read data
     df = get_csvs_df(args.training_data)
 
-    # Split data
+    # split data
     X_train, X_test, y_train, y_test = split_data(df)
-    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
-    # Train model
-    model = train_model(args.reg_rate, X_train, y_train)
+    # train model
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
-    # Evaluate model
-    evaluate_model(model, X_test, y_test)
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -34,35 +34,51 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+
+# TO DO: add function to split data
 def split_data(df, test_size=0.2):
-    if "Diabetic" not in df.columns:
-        raise RuntimeError("Target column 'Diabetic' not found in the DataFrame.")
-    
     X = df.drop("Diabetic", axis=1)
     y = df["Diabetic"]
-    return train_test_split(X, y, test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size
+        )
+    return X_train, X_test, y_train, y_test
 
-def train_model(reg_rate, X_train, y_train):
-    model = LogisticRegression(C=1/reg_rate, solver="liblinear")
-    model.fit(X_train, y_train)
-    return model
 
-def evaluate_model(model, X_test, y_test):
-    predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    print(f"Model Accuracy: {accuracy:.2f}")
+def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    # train model
+    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+
 
 def parse_args():
+    # setup arg parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--training_data", dest='training_data', type=str, required=True)
-    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
-    return parser.parse_args()
 
-# Run script
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+# run script
 if __name__ == "__main__":
+    # add space in logs
     print("\n\n")
     print("*" * 60)
+
+    # parse args
     args = parse_args()
+
+    # run main function
     main(args)
+
+    # add space in logs
     print("*" * 60)
     print("\n\n")
